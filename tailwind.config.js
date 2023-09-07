5bf5c113f9e8f896d243b0140e4c4a63e204523a
usernob <shamsulmaarif2016@gmail.com>
2023-09-07 09:16:55 +0700

    unfinished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html"],
+  content: ['./src/**/*.html'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '3rem',
+        lg: '8rem',
+      },
+    },
+    extend: {
+      colors: {
+        'primary-1': '#F9E0BB',
+        'primary-2': '#FFC26F',
+        'primary-3': '#C38154',
+        'primary-4': '#884A39',
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif', 'system-ui'],
+      },
+    },
   },
   plugins: [],
-}
-
+};
