e776ca1f36930e0a81684d89ef242ac0bf41f540
usernob <usernob@users.noreply.github.com>
2023-09-14 05:20:03 +0000

    deploy: a4f68f3d29b5827726ed1fa2f7322d37af6fba71

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
+const dropdownButton = document.querySelector('#dropdown-button');
+const dropdownMenu = document.querySelector('#dropdown-menu');
+
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
+});
+
+dropdownButton.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('hidden');
 });
