dbd853ad4c6ac97011dbac206297e3c46cd55a99
usernob <usernob@users.noreply.github.com>
2023-09-16 16:06:27 +0000

    deploy: 78bbda079fd3ac385a90d73774f8a57be642fb0a

--- public/js/main.js
+++ public/js/main.js
@@ -3,11 +3,20 @@ const navMenu = document.querySelector('#nav-menu');
 const dropdownButton = document.querySelector('#dropdown-button');
 const dropdownMenu = document.querySelector('#dropdown-menu');
 
+function animatePopUp(element, modifier, statein, stateout, timeout = 100) {
+    element.style[modifier] = statein;
+    setTimeout(() => {
+        element.style[modifier] = stateout;
+    }, timeout);
+}
+
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
+    animatePopUp(navMenu, 'opacity', '0', '1');
 });
 
 dropdownButton.addEventListener('click', () => {
     dropdownMenu.classList.toggle('hidden');
+    animatePopUp(dropdownMenu, 'opacity', '0', '1');
 });
